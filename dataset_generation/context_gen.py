f40aeffcb26769d9dddb541a91a3d0a33a7d2bfd
celishxxp <sunceli@brandeis.edu>
2018-07-02 10:15:59 +0800

    remove header

--- dataset_generation/context_gen.py
+++ dataset_generation/context_gen.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017, BMW Group, all rights reserved.
-#
-# Redistribution and use in source and other forms, with or without modification,
-# are permitted only in BMW internal usage. Any other companies or third-party
-# SHOULD NOT use it.
-#
-# This software is provided by the copyright holders and contributors "as is" and
-# any express or implied warranties, including, but not limited to, the implied
-# warranties of merchantability and fitness for a particular purpose are disclaimed.
-# In no event shall copyright holders or contributors be liable for any direct,
-# indirect, incidental, special, exemplary, or consequential damages
-# (including, but not limited to, procurement of substitute goods or services;
-# loss of use, data, or profits; or business interruption) however caused
-# and on any theory of liability, whether in contract, strict liability,
-# or tort (including negligence or otherwise) arising in any way out of
-# the use of this software, even if advised of the possibility of such damage.
-#
-# @Time    : 18-5-24
-# @Author  : Celi Sun
-# @Superviso: Gavin.Xu (Gavin.Xu@bmw.com)
-# @Department: EG-CN-72
-
 import os
 import numpy as np
 import cv2
